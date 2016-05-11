4fa5321901a3491198b500e17f68a523916b379c
Bartosz Zychal <Bartosz Zychal>
2016-05-11 22:45:44 +0200

    created footer

---
+++ angularjs/wylewki.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var wylewki = angular.module('wylewki');
+
+wylewki.controller('MainCtrl', function($scope, $http){
+
+});
+
+wylewki.controller('AboutCtrl', function($scope, $http){
+
+});
--- angularjs/wylewki.module.js
+++ angularjs/wylewki.module.js
@@ -1,13 +1,32 @@
-angular.module('wylewki',[]);
+'use strict';
 
+var app = angular.module('example359', []).
+    config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.
+        when('/', { templateUrl: 'pages/index.html', activetab: 'projects', controller: HomeCtrl }).
+        when('/project/:projectId', {
+          templateUrl: function (params) { return 'pages/' + params.projectId + '.html'; },
+          controller: MainCtrl,
+          activetab: 'projects'
+        }).
+        when('/privacy', {
+          templateUrl: 'pages/privacy.html',
+          controller: AboutCtrl,
+          activetab: 'privacy'
+        }).
+        when('/about', {
+          templateUrl: 'pages/about.html',
+          controller: AboutCtrl,
+          activetab: 'about'
+        }).
+        otherwise({ redirectTo: '/' });
+    }]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function ($scope, $http, $browser, $timeout, $route) {
 
-//.directive('header', function () {
-//     return {
-    //     restrict: 'A', //This menas that it will be used as an attribute and NOT as an element. I don't like creating custom HTML elements
-    //     replace: true,
-    //     templateUrl: "/src/header.html",
-    //     controller: ['$scope', '$filter', function ($scope, $filter) {
-    //         // Your behaviour goes here :)
-    //     }]
-    // }
-// });
+        $scope.$on("$routeChangeSuccess", function (scope, next, current) {
+          $scope.part = $route.current.activetab;
+        });
+  }]);
+
+app.config(['$locationProvider', function($location) {
+    $location.hashPrefix('!');
+}]);
