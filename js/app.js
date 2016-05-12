0f3fec99cd861f992f5a1c1ef67fb1028bf48980
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-05-12 13:08:26 +0200

    Huraangular worked

--- js/app.js
+++ js/app.js
@@ -1,21 +1,13 @@
+﻿
+
 'use strict';
 
-var app = angular.module('example359', []).
+var app = angular.module('wylewki', []).
     config(['$routeProvider', function ($routeProvider) {
       $routeProvider.
-        when('/', { templateUrl: 'pages/index.html', activetab: 'projects', controller: HomeCtrl }).
-        when('/project/:projectId', {
-          templateUrl: function (params) { return 'pages/' + params.projectId + '.html'; },
-          controller: MainCtrl,
-          activetab: 'projects'
-        }).
-        when('/privacy', {
-          templateUrl: 'pages/privacy.html',
-          controller: AboutCtrl,
-          activetab: 'privacy'
-        }).
-        when('/about', {
-          templateUrl: 'pages/about.html',
+        when('/', { templateUrl: 'src/home.html', activetab: 'home', controller: HomeCtrl }).
+        when('/o_nas', {
+          templateUrl: 'src/o_nas.html',
           controller: AboutCtrl,
           activetab: 'about'
         }).
