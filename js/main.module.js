513d709b8a9e8e56a74e20839cd794c182beffda
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-05-18 13:48:51 +0200

    work

--- js/main.module.js
+++ js/main.module.js
@@ -33,13 +33,14 @@ var app = angular.module('wylewki', ['ngMap','maps','ngRoute','ngAnimate','socia
         }).
         otherwise({ redirectTo: '/' });
     }]).run(['$rootScope', '$http', '$browser', '$timeout', "$route", function ($scope, $http, $browser, $timeout, $route) {
+      $scope.loaded = false;
       $scope.appliedClass = function(myObj) {
-      if (myObj.someValue === "true") {
-        return "special-css-class";
-      } else {
-        return "default-class"; // Or even "", which won't add any additional classes to the element
-      }
-}
+        if ($scope.loaded === "false") {
+          return "";
+        } else {
+          return "slide"; // Or even "", which won't add any additional classes to the element
+        }
+      };
         $scope.$on("$routeChangeSuccess", function (scope, next, current) {
           $scope.part = $route.current.activetab;
         });
